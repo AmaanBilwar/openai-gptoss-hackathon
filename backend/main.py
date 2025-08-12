b61744d483daec8ccfcd567378ebb5214767d834
Amaan Bilwar <bilwarad@mail.uc.edu>
2025-08-12 19:31:29 -0400

    refactor: remove whitespace

--- backend/main.py
+++ backend/main.py
@@ -13,17 +13,9 @@ load_dotenv()
 
 GITHUB_CLIENT_ID = os.getenv("GITHUB_CLIENT_ID")
 GITHUB_CLIENT_SECRET = os.getenv("GITHUB_CLIENT_SECRET")
-
-
-
-
-
 GITHUB_DEVICE_CODE_URL = "https://github.com/login/device/code"
 GITHUB_ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token"
 GITHUB_API_ME_URL = "https://api.github.com/user"
-
-
-
 
 
 def _print_err(message: str) -> None:
